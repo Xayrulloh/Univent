b5ad9a36f546533747f6731fad102ec877b402cb
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2025-09-15 17:46:20 +0500

    fix: bug fixed; chore: formated

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -1,17 +1,17 @@
+import en from '../content/i18n/en.json';
 import ru from '../content/i18n/ru.json';
 import uz from '../content/i18n/uz.json';
-import en from '../content/i18n/en.json';
 
 export const translations = {
   ru,
   uz,
-  en
+  en,
 } as const;
 
 export type Locale = keyof typeof translations;
 
 export function getTranslation(locale: string = 'ru') {
-  const safeLocale = (locale in translations) ? locale as Locale : 'ru';
+  const safeLocale = locale in translations ? (locale as Locale) : 'ru';
 
   return translations[safeLocale];
 }
@@ -19,6 +19,6 @@ export function getTranslation(locale: string = 'ru') {
 export function getCurrentLocale(url: URL): Locale {
   const pathParts = url.pathname.split('/').filter(Boolean);
   const possibleLocale = pathParts[0];
-  
-  return (possibleLocale in translations) ? possibleLocale as Locale : 'ru';
+
+  return possibleLocale in translations ? (possibleLocale as Locale) : 'ru';
 }
