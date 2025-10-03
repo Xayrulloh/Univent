41d9177ad6e7e166f1cbce14d7c8c6cb2f5cb95c
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2025-10-03 16:22:12 +0500

    refactor: sender changed telegram

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const API_URL =
   'https://api.telegram.org/bot7257071849:AAFHpwPl7U2f8McbFExHJh8S8Lzjp34JgVI/sendMessage';
-export const CHAT_ID = 1151533771;
+export const CHAT_ID = 5984256146;
